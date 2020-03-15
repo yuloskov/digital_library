8e1579686fb6694c6ef551618d93d81b00ea5fb1
Yuloskov Artyom <yuloskov.aa@yandex.ru>
2020-03-15 19:17:23 +0300

    Add backend preset for auth

--- web/application/views/auth.py
+++ web/application/views/auth.py
@@ -5,8 +5,18 @@ from flask import (
     url_for,
     render_template,
 )
-import flask_login
+
 from application.utils import DbManager
+
+
+from flask_login import (
+    UserMixin,
+    login_required,
+    current_user,
+    login_user,
+    logout_user,
+)
+
 
 bp = Blueprint('auth', __name__)
 
@@ -16,6 +26,31 @@ def login():
     #DbManager.Manager.connect(self=DbManager.Manager)
     #DbManager.Manager.insert_user(self=DbManager.Manager,login='login',password="password")
     #print(DbManager.Manager.check_validity(self=DbManager.Manager,login='login',password='password'))
+    if current_user.is_authenticated:
+        return redirect(url_for(''))
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    login = request.form['login']
+    user_password = db_manager.get_admins_password(login)
+
+    if user_password is None:
+        return render_template(
+            'login.html',
+            message='Incorrect login or password',
+        )
+
+    match = db_manager.verify_password(
+        user_password,
+        request.form['password']
+    )
+    if match:
+        user = UserMixin()
+        user.id = login
+        login_user(user)
+        return redirect(url_for('report.report'))
+
     return render_template(
         'login.html',
     )
