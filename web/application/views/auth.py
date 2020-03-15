2b5deab7ca1f01d7410f59171d19356216f481b4
moiiwa <moiiwa@icloud.com>
2020-03-15 14:18:55 +0300

    Listed methods added: 1)connect - connection to database 2)hash_password - hash given password 3)insert_user - insert user with given login and password 4)get_user - get user from database 5)verify_password - check if hashed password is equal to password in the database 6)check_validity - check that given login and password exist in database

--- web/application/views/auth.py
+++ web/application/views/auth.py
@@ -5,14 +5,17 @@ from flask import (
     url_for,
     render_template,
 )
-
+import flask_login
+from application.utils import DbManager
 
 bp = Blueprint('auth', __name__)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-
+    #DbManager.Manager.connect(self=DbManager.Manager)
+    #DbManager.Manager.insert_user(self=DbManager.Manager,login='login',password="password")
+    #print(DbManager.Manager.check_validity(self=DbManager.Manager,login='login',password='password'))
     return render_template(
         'login.html',
     )
