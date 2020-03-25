62f7f52ba4576d366a6054d9023ab03813a436ff
Miserable_fx <alecsey.murashko@gmail.com>
2020-03-25 17:55:57 +0300

    Sidebar is Working

--- gulp/js/download.js
+++ gulp/js/download.js
@@ -4,4 +4,19 @@ $(document).ready(function(){
         duration: 700,
         easing: 'swing'
     });
+
+    if ($('#sidebar_category').val() == 'subject'){
+        $('#sidebar_courses').fadeOut();
+    } else {
+        $('#sidebar_subjects').fadeOut();
+    }
+
+    $('#sidebar_category').on("change", function(){
+        if ($('#sidebar_category').val() == 'subject'){
+            $('#sidebar_courses').fadeOut(400, () => $('#sidebar_subjects').fadeIn());
+        } else {
+            $('#sidebar_subjects').fadeOut(400, () => $('#sidebar_courses').fadeIn());
+        }
+    });
+
 });
