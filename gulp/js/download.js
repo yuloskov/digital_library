34eafd8eb4d5b552c336bd3bd8b5fc9c0dc93b87
Miserable_fx <alecsey.murashko@gmail.com>
2020-03-25 22:28:20 +0300

    almost fully working download page

--- gulp/js/download.js
+++ gulp/js/download.js
@@ -1,22 +1,68 @@
 $(document).ready(function(){
+    
+    // Get all important values:
+    let sidebar_category = $('#sidebar_category');
+    let sidebar_subjects = $('#sidebar_subjects');
+    let sidebar_courses = $('#sidebar_courses');
+    let articles = $('.main__content__article');
+    let current_article = 0;
+    let number_of_articles = articles.length;
 
-    $("#hider").fadeOut({
-        duration: 700,
-        easing: 'swing'
+    $('.main__content').bind('mousewheel', function(event) {
+        let next_article;
+        if (event.originalEvent.wheelDelta >= 0) {
+            if (current_article + 1 >= number_of_articles)
+                next_article = 0;
+            else
+                next_article = current_article + 1;
+            
+        } else {
+            if (current_article - 1 < 0)
+                next_article = number_of_articles - 1;
+            else
+                next_article = current_article - 1;
+        }
+
+        $(articles[current_article]).fadeOut(200, () => {
+            $(articles[next_article]).fadeIn(200);
+            current_article = next_article;
+        });
+
     });
 
-    if ($('#sidebar_category').val() == 'subject'){
-        $('#sidebar_courses').fadeOut();
-    } else {
-        $('#sidebar_subjects').fadeOut();
-    }
+    // Page is loaded, display it:
+    initialize();
 
-    $('#sidebar_category').on("change", function(){
-        if ($('#sidebar_category').val() == 'subject'){
-            $('#sidebar_courses').fadeOut(400, () => $('#sidebar_subjects').fadeIn());
+    // Sidebar Animation:
+    sidebar_category.on("change", function(){
+        if (sidebar_category.val() == 'subject'){
+            sidebar_courses.fadeOut(400, () => sidebar_subjects.fadeIn());
         } else {
-            $('#sidebar_subjects').fadeOut(400, () => $('#sidebar_courses').fadeIn());
+            sidebar_subjects.fadeOut(400, () => sidebar_courses.fadeIn());
         }
     });
 
 });
+
+function initialize(){
+    let sidebar_list;
+    let articles;
+
+    articles = $('.main__content__article');
+    articles.hide(1, ()=>{
+        $(articles[0]).fadeIn(1);
+    });
+
+    if ($('#sidebar_category').val() == 'subject')
+        sidebar_list = $('#sidebar_courses');
+    else 
+        sidebar_list = $('#sidebar_subjects');
+
+        sidebar_list.hide(100, ()=>{
+            $("#hider").fadeOut({
+                duration: 400,
+                easing: 'swing'
+            });
+        });
+
+}
