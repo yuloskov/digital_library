c671facab880647a7dcaf7c80d2600e5753ccdac
Miserable_fx <alecsey.murashko@gmail.com>
2020-03-26 11:57:13 +0300

    upload page created

--- gulp/js/download.js
+++ gulp/js/download.js
@@ -5,9 +5,14 @@ $(document).ready(function(){
     let sidebar_subjects = $('#sidebar_subjects');
     let sidebar_courses = $('#sidebar_courses');
     let articles = $('.main__content__article');
+    let upload = $('.main__content__article_upload');
+    let upload_button = $('.footer__upload_text');
     let current_article = 0;
     let number_of_articles = articles.length;
+    var upload_visible = false;
 
+
+    // Show Articles
     $('.main__content').bind('mousewheel', function(event) {
         let next_article;
         if (event.originalEvent.wheelDelta >= 0) {
@@ -23,11 +28,48 @@ $(document).ready(function(){
                 next_article = current_article - 1;
         }
 
-        $(articles[current_article]).fadeOut(200, () => {
-            $(articles[next_article]).fadeIn(200);
+        $(articles[current_article]).fadeOut(250, () => {
+            $(articles[next_article]).fadeIn(250);
             current_article = next_article;
         });
 
+    });
+
+    // Show Upload Page 
+    upload_button.on("click", () => {
+        if (upload_visible){
+            upload.fadeOut(250, () => {
+                $(articles[current_article]).fadeIn(250);
+                upload_visible = false;
+                $('.main__content').on("mousewheel", function(event) {
+                    let next_article;
+                    if (event.originalEvent.wheelDelta >= 0) {
+                        if (current_article + 1 >= number_of_articles)
+                            next_article = 0;
+                        else
+                            next_article = current_article + 1;
+                        
+                    } else {
+                        if (current_article - 1 < 0)
+                            next_article = number_of_articles - 1;
+                        else
+                            next_article = current_article - 1;
+                    }
+            
+                    $(articles[current_article]).fadeOut(250, () => {
+                        $(articles[next_article]).fadeIn(250);
+                        current_article = next_article;
+                    });
+            
+                });
+            });
+        } else{
+            $(articles[current_article]).fadeOut(250, () => {
+                upload.fadeIn(250);
+                upload_visible = true;
+                $('.main__content').off("mousewheel");
+            });
+        }
     });
 
     // Page is loaded, display it:
@@ -36,10 +78,32 @@ $(document).ready(function(){
     // Sidebar Animation:
     sidebar_category.on("change", function(){
         if (sidebar_category.val() == 'subject'){
-            sidebar_courses.fadeOut(400, () => sidebar_subjects.fadeIn());
+            sidebar_courses.fadeOut(250, () => sidebar_subjects.fadeIn());
         } else {
-            sidebar_subjects.fadeOut(400, () => sidebar_courses.fadeIn());
+            sidebar_subjects.fadeOut(250, () => sidebar_courses.fadeIn());
         }
+    });
+
+    // File download animation
+    var inputs = document.querySelectorAll( '.inputfile' );
+    Array.prototype.forEach.call( inputs, function( input )
+    {
+        var label	 = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener( 'change', function( e )
+        {
+            var fileName = '';
+            if( this.files && this.files.length > 1 )
+                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+            else
+                fileName = e.target.value.split('\\').pop();
+
+            if( fileName )
+                label.innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
     });
 
 });
@@ -51,6 +115,7 @@ function initialize(){
     articles = $('.main__content__article');
     articles.hide(1, ()=>{
         $(articles[0]).fadeIn(1);
+        $('.main__content__article_upload').hide();
     });
 
     if ($('#sidebar_category').val() == 'subject')
