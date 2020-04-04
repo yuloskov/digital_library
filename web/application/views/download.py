d575aaf3c6d94ae8e733190fabd3f1d3d2aa3b73
Yuloskov Artyom <yuloskov.aa@yandex.ru>
2020-04-04 12:58:58 +0300

    Add list generation in backend

--- web/application/views/download.py
+++ web/application/views/download.py
@@ -6,11 +6,28 @@ from flask import (
     render_template,
 )
 
+from application.utils import DbManager
+
 bp = Blueprint('download', __name__)
+
 
 @bp.route('/download', methods=['GET', 'POST'])
 def download():
+    db_manager = DbManager.Manager()
+    # books = db_manager.get_list_of_books()
+    books = [
+        {'_id': 'bookid1',
+         'title': '1',
+         'description': 'desc',
+         'img': 'Ross_Networks.jpg'
+         },
+        {'_id': 'bookid2',
+         'title': '2',
+         'description': 'desc2',
+         'img': 'Russell_AI.jpg'
+         },
+    ]
     return render_template(
         'download.html',
-        message='Incorrect login or password',
+        books=books,
     )
